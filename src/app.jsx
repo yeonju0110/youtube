dec2a4d3ae0102778b85443393530618e8ff24b0
Yeonju Jo <yjj0287@gmail.com>
2022-05-23 00:30:55 +0900

    add responsive mode and clean up

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoDetail from './components/video_detail/video_detail';
@@ -12,20 +12,21 @@ function App({ youtube }) {
     setSelectedVideo(video);
   };
 
-  const search = query => {
-    setSelectedVideo(null);
-    youtube
-      .search(query)
-      .then(videos => {
-          setVideos(videos);
-      });
-  };
+  const search = useCallback(
+    query => {
+      setSelectedVideo(null);
+      youtube
+        .search(query)
+        .then(videos => setVideos(videos));
+    },
+    [youtube]
+  );
 
   useEffect(()=>{
     youtube
       .mostPopular()
       .then(videos => setVideos(videos));
-  }, []);
+  }, [youtube]);
 
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app.jsx';
 import Youtube from './service/youtube';
+import axios from 'axios';
+
+document.cookie = "safeCookie1=foo; SameSite=Lax";
+document.cookie = "safeCookie2=foo";
+document.cookie = "crossCokie=bar; SameSite=None; Secure";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
+const httpClient = axios.create({
+  baseURL: 'https://youtube.googleapis.com/youtube/v3',
+  params: {key: process.env.REACT_APP_YOUTUBE_API_KEY},
+});
+const youtube = new Youtube(httpClient);
+
 root.render(
   <React.StrictMode>
     <App youtube={youtube}/>
