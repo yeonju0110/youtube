1952cc4ddf7bffebe62c8b997785b24adff540a1
Yeonju Jo <yjj0287@gmail.com>
2022-05-22 19:02:50 +0900

    add youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -1,23 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const search = query => {
+    youtube // 한줄에 하나씩이 좋음
+      .search(query)
+      .then(videos => setVideos(videos));
+  };
 
   useEffect(()=>{
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-    
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyANgIXzlCknvL99xFpqnlN7SQhHN9f2_yY", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube
+      .mostPopular()
+      .then(videos => setVideos(videos));
   }, []);
 
-  return <VideoList videos={videos} />;
+  return (
+    <>
+      <div className={styles.app}>
+        <SearchHeader onSearch={search} />
+        <VideoList videos={videos} />
+      </div>
+    </>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app.jsx';
+import Youtube from './service/youtube';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 root.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube}/>
   </React.StrictMode>
 );
